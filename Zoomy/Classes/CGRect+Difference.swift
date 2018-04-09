06be3aca0eeb5974f658f7fa8b0e20fb38b86502
Menno Lovink <mclovink@me.com>
2018-04-09 17:05:20 +0200

    Separate CGRect extension from ImageZoomController

---
+++ Zoomy/Classes/CGRect+Difference.swift
@@ -0,0 +1,17 @@
+//
+//  CGRect+Difference.swift
+//  Pods-Zoomy_Example
+//
+//  Created by Menno on 09/04/2018.
+//
+
+import Foundation
+
+internal extension CGRect {
+    func difference(with rect: CGRect) -> CGRect {
+        return CGRect(x: origin.x - rect.origin.x,
+                      y: origin.y - rect.origin.y,
+                      width: size.width - rect.size.width,
+                      height: size.height - rect.size.height)
+    }
+}
