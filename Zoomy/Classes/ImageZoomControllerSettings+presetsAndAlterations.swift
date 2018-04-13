c3eede401353f0c6cf7225aba70a7c5be0ca92c8
lvnkmn <2656075+lvnkmn@users.noreply.github.com>
2018-04-13 12:54:35 +0200

    closes #10 - Introduce single object that contains all settings for ImageZoomController

---
+++ Zoomy/Classes/ImageZoomControllerSettings+presetsAndAlterations.swift
@@ -0,0 +1,63 @@
+//
+//  ImageZoomControllerSettings.swift
+//  Pods-Zoomy_Example
+//
+//  Created by Menno on 09/04/2018.
+//
+
+import Foundation
+
+// MARK: Presets
+public extension ImageZoomControllerSettings {
+    static var defaultSettings: ImageZoomControllerSettings {
+        return ImageZoomControllerSettings()
+    }
+    
+    static var noBackgroundSettings: ImageZoomControllerSettings {
+        return defaultSettings.with(shouldDisplayBackground: false)
+    }
+    
+    static var noZoomcancellingSettings: ImageZoomControllerSettings {
+        return defaultSettings.with(zoomCancelingThreshold: 1)
+    }
+}
+
+//MARK: - Alterations
+public extension ImageZoomControllerSettings {
+    
+    func with(zoomCancelingThreshold: ImageViewScale) -> ImageZoomControllerSettings {
+        var settings = self
+        settings.zoomCancelingThreshold = zoomCancelingThreshold
+        return self
+    }
+    
+    func with(maximumZoomScale: ImageScale) -> ImageZoomControllerSettings {
+        var settings = self
+        settings.maximumZoomScale = maximumZoomScale
+        return self
+    }
+    
+    func with(isEnabled: Bool) -> ImageZoomControllerSettings {
+        var settings = self
+        settings.isEnabled = isEnabled
+        return self
+    }
+    
+    func with(shouldDisplayBackground: Bool) -> ImageZoomControllerSettings {
+        var settings = self
+        settings.shouldDisplayBackground = shouldDisplayBackground
+        return self
+    }
+    
+    func with(backgroundColorWhenContentIsSmallerThanViewItsDisplayedIn: UIColor) -> ImageZoomControllerSettings {
+        var settings = self
+        settings.backgroundColorWhenContentIsSmallerThanViewItsDisplayedIn = backgroundColorWhenContentIsSmallerThanViewItsDisplayedIn
+        return self
+    }
+    
+    func with(backgroundWhenContentFillsViewItsDisplayedIn: UIColor) -> ImageZoomControllerSettings {
+        var settings = self
+        settings.backgroundWhenContentFillsViewItsDisplayedIn = backgroundWhenContentFillsViewItsDisplayedIn
+        return self
+    }
+}
