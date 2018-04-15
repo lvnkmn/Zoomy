5b3f2d4cd4f70f3a68813bdb5891d3a4d1cf36c5
lvnkmn <2656075+lvnkmn@users.noreply.github.com>
2018-04-15 14:48:50 +0200

    #14 - Add example for non centered images, tweak background colors

--- Zoomy/Classes/ImageZoomControllerSettings.swift
+++ Zoomy/Classes/ImageZoomControllerSettings.swift
@@ -25,8 +25,8 @@ public struct ImageZoomControllerSettings {
     
     /// BackgroundView's color will animate to this value when content becomes smaller than the view it's displayed in
     /// This will only have effect when shouldDisplayBackground is set to true
-    public var backgroundColorWhenContentIsSmallerThanViewItsDisplayedIn = UIColor.black.withAlphaComponent(0.5)
+    public var backgroundColorWhenContentIsSmallerThanViewItsDisplayedIn = UIColor.white.withAlphaComponent(0.8)
     /// BackgroundView's color will animate to this value when content becomes bigger than or equal to any dimension of the view it's displayed in
     /// This will only have effect when shouldDisplayBackground is set to true
-    public var backgroundWhenContentFillsViewItsDisplayedIn = UIColor.black
+    public var backgroundWhenContentFillsViewItsDisplayedIn = UIColor.white
 }
