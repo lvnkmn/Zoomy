742ed1c5acefdb315a9d4fa5b8334d46b30190f5
Menno Lovink <mclovink@me.com>
2018-04-16 11:45:43 +0200

    closes #17 - Simplify API for UIViewControllers

---
+++ Example/Tests/AssertionMessages.swift
@@ -0,0 +1,14 @@
+//
+//  AssertionMessages.swift
+//  Zoomy_Example
+//
+//  Created by Menno on 16/04/2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    static let expectedProvidedValue = "Should have been provided value"
+    static let expectedDefaultValue = "Should have been default value"
+}
---
+++ Example/Tests/ViewControllerThatIsZoomDelegateTests.swift
@@ -0,0 +1,60 @@
+import XCTest
+@testable import Zoomy
+
+class ViewControllerThatIsZoomDelegateTests: XCTestCase {
+    
+    let sut = MockViewControllerThatIsZoomDelegate()
+    
+    lazy var nonDefaultSettings = ZoomSettings.defaultSettings.with(zoomCancelingThreshold: 1.21231235)
+    lazy var fakeZoomDelegate = FakeZoomDelegate()
+
+    override func tearDown() {
+        sut.imageZoomControllers = [UIImageView: ImageZoomController]()
+        super.tearDown()
+    }
+    
+    // MARK: Tests
+    func testAddBehaviorForImageViewInViewContainerViewWithSettings() {
+        //Act
+        sut.addZoombehavior(for: sut.imageView, in: sut.otherView, settings: nonDefaultSettings)
+        
+        //Assert
+        XCTAssert(sut.imageZoomControllers[sut.imageView]?.imageView === sut.imageView, Message.expectedProvidedValue)
+        XCTAssert(sut.imageZoomControllers[sut.imageView]?.containerView === sut.otherView, Message.expectedProvidedValue)
+        XCTAssert(sut.imageZoomControllers[sut.imageView]?.settings == nonDefaultSettings, Message.expectedProvidedValue)
+        XCTAssert(sut.imageZoomControllers[sut.imageView]?.delegate === sut, Message.expectedDefaultValue)
+    }
+    
+    func testAddBehaviorForImageViewInViewContainer() {
+        //Act
+        sut.addZoombehavior(for: sut.imageView, in: sut.otherView)
+        
+        //Assert
+        XCTAssert(sut.imageZoomControllers[sut.imageView]?.imageView === sut.imageView, Message.expectedProvidedValue)
+        XCTAssert(sut.imageZoomControllers[sut.imageView]?.containerView === sut.otherView, Message.expectedProvidedValue)
+        XCTAssert(sut.imageZoomControllers[sut.imageView]?.settings == .defaultSettings, Message.expectedDefaultValue)
+        XCTAssert(sut.imageZoomControllers[sut.imageView]?.delegate === sut, Message.expectedDefaultValue)
+    }
+
+    func testAddBehaviorForImageViewWithSettings() {
+        //Act
+        sut.addZoombehavior(for: sut.imageView, settings: nonDefaultSettings)
+        
+        //Assert
+        XCTAssert(sut.imageZoomControllers[sut.imageView]?.imageView === sut.imageView, Message.expectedProvidedValue)
+        XCTAssert(sut.imageZoomControllers[sut.imageView]?.containerView === sut.view, Message.expectedDefaultValue)
+        XCTAssert(sut.imageZoomControllers[sut.imageView]?.settings == nonDefaultSettings, Message.expectedProvidedValue)
+        XCTAssert(sut.imageZoomControllers[sut.imageView]?.delegate === sut, Message.expectedDefaultValue)
+    }
+    
+    func testAddBehaviorForImageView() {
+        //Act
+        sut.addZoombehavior(for: sut.imageView)
+        
+        //Assert
+        XCTAssert(sut.imageZoomControllers[sut.imageView]?.imageView === sut.imageView, Message.expectedProvidedValue)
+        XCTAssert(sut.imageZoomControllers[sut.imageView]?.containerView === sut.view, Message.expectedDefaultValue)
+        XCTAssert(sut.imageZoomControllers[sut.imageView]?.settings == ZoomSettings.defaultSettings, Message.expectedDefaultValue)
+        XCTAssert(sut.imageZoomControllers[sut.imageView]?.delegate === sut, Message.expectedDefaultValue)
+    }
+}
