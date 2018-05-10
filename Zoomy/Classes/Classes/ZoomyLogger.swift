81042206a698964e68fd975fba74f5dcccadbec1
lvnkmn <2656075+lvnkmn@users.noreply.github.com>
2018-05-10 14:10:52 +0200

    fix mistake

--- Zoomy/Classes/Classes/ZoomyLogger.swift
+++ Zoomy/Classes/Classes/ZoomyLogger.swift
@@ -4,7 +4,7 @@ import InjectableLoggers
 
 internal class ZoomyLogger: Logger {
     
-    internal func logGesture(with gestureRecognizer: UIGestureRecognizer, atLevel level: Loglevel? = nil, inFile file: String? = #file, inFunction function: String? = #function, atLine line: Int? = #line) {
+    internal func logGesture(with gestureRecognizer: UIGestureRecognizer, atLevel level: Loglevel, inFile file: String? = #file, inFunction function: String? = #function, atLine line: Int? = #line) {
         guard gestureRecognizer.state != .changed else { return }
         
         log(gestureRecognizer.state, atLevel: level, inFile: file, inFunction: function, atLine: line)
