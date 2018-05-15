de4d2f8344fe1c23a30791e82a607d0089e0b184
Menno Lovink <mclovink@me.com>
2018-05-15 18:15:40 +0200

    closes #42 - Improve `ChangingActionsViewController` by adding the possibility to zoomToFit

--- Example/Zoomy/ChangingActionsViewController.swift
+++ Example/Zoomy/ChangingActionsViewController.swift
@@ -11,6 +11,8 @@ import Zoomy
 
 class ChangingActionsViewController: UIViewController {
 
+    @IBOutlet weak var tapImageViewSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var doubleTapImageViewSegmentedControl: UISegmentedControl!
     @IBOutlet weak var tapOverlaySegmentedControl: UISegmentedControl!
     @IBOutlet weak var scrollBounceTopSegmentedControl: UISegmentedControl!
     @IBOutlet weak var scrollBounceLeftSegmentedControl: UISegmentedControl!
@@ -19,14 +21,21 @@ class ChangingActionsViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     
+    private let overlayActions = [Action.none, Action.dismissOverlay]
+    private let imageViewActions = [Action.none, Action.zoomToFit]
+    
     var settings: Settings {
         var settings: Settings = .backgroundEnabledSettings
         
+        settings.actionOnTapImageView = action(for: tapImageViewSegmentedControl) as? Action & CanBeTriggeredByImageViewTap ?? Action.none
+        settings.actionOnDoubleTapImageVIew = action(for: doubleTapImageViewSegmentedControl) as? Action & CanBeTriggeredByImageViewDoubleTap ?? Action.none
         settings.actionOnTapOverlay = action(for: tapOverlaySegmentedControl) as? Action & CanBeTriggeredByOverlayTap ?? Action.none
         settings.actionOnScrollBounceTop = action(for: scrollBounceTopSegmentedControl) as? Action & CanBeTriggeredByScrollBounceTop ?? Action.none
         settings.actionOnScrollBounceLeft = action(for: scrollBounceLeftSegmentedControl) as? Action & CanBeTriggeredByScrollBounceLeft ?? Action.none
         settings.actionOnScrollBounceRight = action(for: scrollBounceRightSegmentedControl) as? Action & CanBeTriggeredByScrollBounceRight ?? Action.none
         settings.actionOnScrollBounceBottom = action(for: scrollBounceBottomSegmentedControl) as? Action & CanBeTriggeredByScrollBounceBottom ?? Action.none
+        
+        settings.secundaryBackgroundColor = UIColor.black.withAlphaComponent(0.6)
         
         return settings
     }
@@ -49,7 +58,24 @@ class ChangingActionsViewController: UIViewController {
 private extension ChangingActionsViewController {
     
     func action(for segmentedControl: UISegmentedControl) -> Action {
-        let actions = [Action.none, Action.dismissOverlay]
-        return actions[segmentedControl.selectedSegmentIndex]
+        return actions(for: segmentedControl)[segmentedControl.selectedSegmentIndex]
+    }
+    
+    func actions(for segmentedControl: UISegmentedControl) -> [Action] {
+        if imageViewSegmentedControls.contains(segmentedControl) {
+            return imageViewActions
+        } else if overlaySegmentedControls.contains(segmentedControl) {
+            return overlayActions
+        } else {
+            return []
+        }
+    }
+    
+    var imageViewSegmentedControls: [UISegmentedControl] {
+        return [tapImageViewSegmentedControl, doubleTapImageViewSegmentedControl]
+    }
+    
+    var overlaySegmentedControls: [UISegmentedControl] {
+        return [tapOverlaySegmentedControl, scrollBounceTopSegmentedControl, scrollBounceLeftSegmentedControl, scrollBounceRightSegmentedControl, scrollBounceBottomSegmentedControl]
     }
 }
