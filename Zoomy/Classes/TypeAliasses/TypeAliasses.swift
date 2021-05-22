1ed7bb1cba7febee170b4898baf776293c533c79
menno <mnlvnkgit@gmail.com>
2021-05-22 20:56:34 +0200

    Fix typo's in documentation

--- Zoomy/Classes/TypeAliasses/TypeAliasses.swift
+++ Zoomy/Classes/TypeAliasses/TypeAliasses.swift
@@ -11,7 +11,7 @@ import UIKit
 
 /// The scale transformation that is applied on an imageView.
 /// The value of 1 results in the original view size.
-/// The value of 2 retulst in twice the original view width and height.
+/// The value of 2 retults in twice the original view width and height.
 /// The value of 0.5 results in half the original view width and height etc.
 ///
 /// Note that ImageViewScale is different from the scale of the image, see ImageScale.
@@ -19,7 +19,7 @@ public typealias ImageViewScale = CGFloat
 
 /// The scale transformation that is applied on an image.
 /// The value of 1 results in the image size -> 1 pixel per point on screen.
-/// The value of 2 retulst in twice the original view width and height -> 2 pixels per point on screen.
+/// The value of 2 retults in twice the original view width and height -> 2 pixels per point on screen.
 /// The value of 0.5 results in half the original view width and height -> 0.5 pixels per point on screen etc.
 ///
 /// Note that ImageScale is different from the scale of the imageView, see ImageViewScale.
